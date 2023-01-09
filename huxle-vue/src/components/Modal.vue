0785edd859f9ced358e44af0148462038a7d271e
MartinS <sitummartin@gmail.com>
2023-01-09 23:55:51 +0100

    Modal component

---
+++ huxle-vue/src/components/Modal.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">import { ref } from '@vue/reactivity';
+
+let modalOpen = ref(true);
+
+let emojis = ["🎉","😢"]
+
+const props = defineProps<{
+  // keyLetter: Letter;
+  open: boolean,
+  message: string,
+  closeable: boolean,
+  emojiID: number
+}>();
+
+</script>
+
+<template>
+    <!--Modal-->
+        <div v-show="modalOpen" class="absolute inset-0 flex items-center justify-center z-10 ">
+            <div class="max-w-2xl p-6 rounded-lg shadow-xl bg-slate-800 z-10 w-2/4 h-2/5">
+                <div class="flex items-center justify-between w-full">
+                    <h3 class="text-4xl flex-auto mx-auto text-center text-slate-100 px-auto">{{ message }}</h3>
+                    <svg v-if="closeable" @click="modalOpen = false" xmlns="http://www.w3.org/2000/svg"
+                        class="w-6 h-6 flex-none text-red-900 cursor-pointer" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3"
+                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </div>
+                <div>
+                    <h1 class="w-full h-full text-8xl text-center text pt-10 mx-auto px-auto">{{ emojis[emojiID] }}</h1>
+                </div>
+            </div>
+        </div>
+</template>
+<style>
+
+</style>
--- huxle-vue/src/views/HomeView.vue
+++ huxle-vue/src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import Keyboard from "../components/keyboard/Keyboard.vue";
 import GuessBox from "@/components/guessbox/GuessBox.vue";
+import Modal from "@/components/Modal.vue"
+import { ref } from "@vue/reactivity";
+
+let modalOpen = ref(false);
 </script>
 
 <template>
   <div>
     <guess-box></guess-box>
     <Keyboard />
+    <Modal :open="modalOpen" :closeable=false message="You win!" :emojiID=0></Modal>
   </div>
 </template>
